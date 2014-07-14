bce472edaaf686e815592fb7e049ee2c35517fba
petegoo <pete@petegoo.com>
2014-07-14 17:02:02 +1200

    Initial commit

---
+++ FileWatcherRepro/FileWatcherImplementation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace FileWatcherRepro
+{
+    public class FileWatcherImplementation
+    {
+        private FileSystemWatcher _watcher;
+
+        public FileWatcherImplementation()
+        {
+            _watcher = new FileSystemWatcher("temp");
+            _watcher.EnableRaisingEvents = true;
+
+            _watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.LastWrite | NotifyFilters.Size;
+
+            _watcher.Created += (sender, args) =>
+            {
+                FileUpdated(args.ChangeType, args.FullPath);
+            };
+
+            _watcher.Changed += (sender, args) =>
+            {
+                FileUpdated(args.ChangeType, args.FullPath);
+            };
+        }
+
+        private void FileUpdated(WatcherChangeTypes changeType, string fullPath)
+        {
+            Console.WriteLine("ChangeType: {0}, FullPath: {1}: Size: {2}",
+                changeType,
+                fullPath,
+                new FileInfo(fullPath).Length);
+        }
+    }
+}
---
+++ FileWatcherRepro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileWatcherRepro
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var watcher = new FileWatcherImplementation();
+
+            while(true)
+            {
+                Console.ReadKey(true);
+
+                Console.WriteLine("Press any key to create a file");
+                Task.Factory.StartNew(WriteFileSlow);
+            }
+        }
+
+        private static void WriteFileQuick()
+        {
+            File.WriteAllBytes(Path.Combine("temp", Guid.NewGuid() + ".bin"), new Byte[100000]);
+        }
+
+        private static void WriteFileSlow()
+        {
+            using (var stream = File.Open(Path.Combine("temp", Guid.NewGuid() + ".bin"), FileMode.CreateNew))
+            {
+                stream.Write(new Byte[50000], 0, 50000);
+                stream.Flush(true);
+                Task.Delay(TimeSpan.FromMilliseconds(5000)).Wait();
+                stream.Write(new Byte[50000], 0, 50000);
+            }
+        }
+    }
+}
